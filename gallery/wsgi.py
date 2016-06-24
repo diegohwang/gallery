e6679dd26fb333e8801f0ce113b67d1e90d9fcaa
diegohwang <diegohwang@163.com>
2016-06-24 17:22:10 +0800

    commit wsgi.py

--- gallery/wsgi.py
+++ gallery/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gallery.settings")
 
 application = get_wsgi_application()
+
+import sys
+
+sys.path.append("/home/huangjm/study/mydjango/gallery")
